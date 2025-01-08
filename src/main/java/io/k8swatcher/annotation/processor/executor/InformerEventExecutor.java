f52d8b6bb69672cbba559b053e56225b5ddb8d7c
Viktor Pergjoka <viktor.pergjoka@gmail.com>
2025-01-08 22:04:52 +0100

    Added Executor which adds tasks and executes them in the order they arrive

--- src/main/java/io/k8swatcher/annotation/processor/executor/InformerEventExecutor.java
+++ src/main/java/io/k8swatcher/annotation/processor/executor/InformerEventExecutor.java
@@ -1,27 +1,18 @@
 package io.k8swatcher.annotation.processor.executor;
 
 import java.util.Queue;
-import java.util.concurrent.*;
-import lombok.Getter;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingDeque;
 
-/**
- * <br>
- * This is a modified version of https://github.com/fabric8io/kubernetes-client/blob/076a981c9845c691a3d45a727fd48e03377abe8f/kubernetes-client/src/main/java/io/fabric8/kubernetes/client/utils/internal/SerialExecutor.java
- * <br>
- */
 public class InformerEventExecutor implements Executor {
 
     private final Queue<Runnable> tasks = new LinkedBlockingDeque<>();
 
     private final ExecutorService executor;
+
     private Runnable active;
-
-    @Getter
-    private volatile boolean shutdown;
-
-    private Thread thread;
-
-    private final Object threadLock = new Object();
 
     public InformerEventExecutor(ExecutorService executor) {
         this.executor = executor;
@@ -29,27 +20,9 @@ public class InformerEventExecutor implements Executor {
 
     @Override
     public synchronized void execute(final Runnable r) {
-        if (shutdown) {
-            throw new RejectedExecutionException();
-        }
         tasks.offer(() -> {
-            try {
-                if (shutdown) {
-                    return;
-                }
-                synchronized (threadLock) {
-                    thread = Thread.currentThread();
-                }
-                CompletableFuture.runAsync(r);
-            } catch (Throwable t) {
-                thread.getUncaughtExceptionHandler().uncaughtException(thread, t);
-            } finally {
-                synchronized (threadLock) {
-                    thread = null;
-                }
-                Thread.interrupted();
-                scheduleNext();
-            }
+            CompletableFuture.runAsync(r);
+            scheduleNext();
         });
         if (active == null) {
             scheduleNext();
@@ -57,7 +30,8 @@ public class InformerEventExecutor implements Executor {
     }
 
     protected synchronized void scheduleNext() {
-        if ((active = tasks.poll()) != null) {
+        active = tasks.poll();
+        if (active != null) {
             executor.execute(active);
         }
     }
