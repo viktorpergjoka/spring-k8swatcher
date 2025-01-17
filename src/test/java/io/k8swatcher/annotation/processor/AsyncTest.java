58004e03d9b0ae1f641fef2b7e48cbd7944a0f9e
Viktor Pergjoka <viktor.pergjoka@gmail.com>
2025-01-17 23:26:45 +0100

    spotless

--- src/test/java/io/k8swatcher/annotation/processor/AsyncTest.java
+++ src/test/java/io/k8swatcher/annotation/processor/AsyncTest.java
@@ -1,30 +1,9 @@
 package io.k8swatcher.annotation.processor;
 
-import static org.junit.Assert.assertEquals;
-
-import io.fabric8.kubernetes.api.model.Namespace;
-import io.fabric8.kubernetes.api.model.NamespaceBuilder;
-import io.fabric8.kubernetes.api.model.Pod;
-import io.fabric8.kubernetes.client.KubernetesClient;
-import io.fabric8.kubernetes.client.informers.SharedIndexInformer;
-import io.k8swatcher.annotation.cfg.InformerConfigurationProperty;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.IntStream;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-
-//@SpringBootTest(classes = {InformerTestConfig.class})
+// @SpringBootTest(classes = {InformerTestConfig.class})
 public class AsyncTest {
 
-   /* @Autowired
+    /* @Autowired
     private ApplicationContext testCtx;
 
     private KubeClientFactory clientFactory;
