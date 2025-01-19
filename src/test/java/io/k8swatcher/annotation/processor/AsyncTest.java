328d05fbce919a05d8892f98ce99fb450dd5ac24
Viktor Pergjoka <viktor.pergjoka@gmail.com>
2025-01-19 21:27:18 +0100

    readded AsyncTest

--- src/test/java/io/k8swatcher/annotation/processor/AsyncTest.java
+++ src/test/java/io/k8swatcher/annotation/processor/AsyncTest.java
@@ -1,9 +1,30 @@
 package io.k8swatcher.annotation.processor;
 
-// @SpringBootTest(classes = {InformerTestConfig.class})
+import static org.junit.Assert.assertEquals;
+
+import io.fabric8.kubernetes.api.model.Namespace;
+import io.fabric8.kubernetes.api.model.NamespaceBuilder;
+import io.fabric8.kubernetes.api.model.Pod;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.informers.SharedIndexInformer;
+import io.k8swatcher.annotation.cfg.InformerConfigurationProperty;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootTest(classes = {InformerTestConfig.class})
 public class AsyncTest {
 
-    /* @Autowired
+    @Autowired
     private ApplicationContext testCtx;
 
     private KubeClientFactory clientFactory;
@@ -52,7 +73,7 @@ public class AsyncTest {
                 .load(getClass().getResourceAsStream("/events.yml"))
                 .item();
 
-        IntStream.range(0, 2).forEach((index) -> {
+        IntStream.range(0, 5).forEach((index) -> {
             pods.add(client.pods()
                     .resource(pod.edit()
                             .editMetadata()
@@ -66,8 +87,7 @@ public class AsyncTest {
                 .waitUntilCondition(p -> p.getStatus().getPhase().equals("Running"), 1, TimeUnit.MINUTES));
         client.resourceList(pods).delete();
         client.resourceList(pods).waitUntilCondition(Objects::isNull, 1, TimeUnit.MINUTES);
-        Thread.sleep(2000);
-        assertEquals(6, eventsCount.get());
+        Thread.sleep(5000);
+        assertEquals(15, eventsCount.get());
     }
-    */
 }
