6e5c2fc0f82b9f8c238db6e10c76e3d5d2aa554d
viktorpergjoka <viktor.pergjoka@gmail.com>
2025-07-29 17:11:10 +0200

    add apache license

--- spotless.license.java
+++ spotless.license.java
@@ -13,28 +13,3 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.k8swatcher.annotation.cfg;
-
-import io.fabric8.kubernetes.client.KubernetesClient;
-import io.k8swatcher.annotation.Informer;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Setter;
-
-@Data
-@AllArgsConstructor
-public class InformerContext {
-
-    @Setter(AccessLevel.NONE)
-    private Class<?> beanClass;
-
-    @Setter(AccessLevel.NONE)
-    private Informer informer;
-
-    @Setter(AccessLevel.NONE)
-    private InformerConfiguration cfg;
-
-    @Setter(AccessLevel.NONE)
-    private KubernetesClient client;
-}
